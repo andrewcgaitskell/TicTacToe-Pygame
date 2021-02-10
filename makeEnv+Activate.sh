dd9fd7d0e788bf7fbf9584f8e496ba1f0b8882f4
andrewcgaitskell <andrew@gaitskell.com>
2021-02-10 11:40:09 +0000

    Update makeEnv+Activate.sh

--- makeEnv+Activate.sh
+++ makeEnv+Activate.sh
@@ -5,3 +5,9 @@ python3 -m venv env
 source $PWD/env/bin/activate
 
 pip install pygame
+
+https://codeburst.io/building-your-first-chat-application-using-flask-in-7-minutes-f98de4adfa5d
+
+pip install flask
+
+
