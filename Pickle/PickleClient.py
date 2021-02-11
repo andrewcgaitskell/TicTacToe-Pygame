c6ff9e927c33514852d6a888d3bd4fc02effb0c9
andrewcgaitskell <andrew@gaitskell.com>
2021-02-11 09:26:50 +0000

    Update PickleClient.py

--- Pickle/PickleClient.py
+++ Pickle/PickleClient.py
@@ -8,7 +8,11 @@ PORT = 5007              # The same port as used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    #s.sendall(b'Hello, world')
+    playerid = 3
+    position = 1
+    ge = ['play', playerid, position]
+    s.send(pickle.dumps(ge))
     data = s.recv(1024)
 
 print('Received', repr(data))
