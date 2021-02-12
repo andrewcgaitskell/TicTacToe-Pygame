6f57f3e035876c2d6e046d89ede83a28141df0b1
andrewcgaitskell <andrew@gaitskell.com>
2021-02-12 21:03:03 +0000

    Create pips.sh

---
+++ Build/pips.sh
@@ -0,0 +1,19 @@
+pip install pygame
+
+pip install flask
+
+pip install flask-socketio
+
+pip install gunicorn
+
+pip install eventlet
+
+pip install pandas
+
+pip install flask-bootstrap
+
+pip install sockets
+
+pip install "python-socketio[client]"
+
+pip install "python-socketio[asyncio_client]"
