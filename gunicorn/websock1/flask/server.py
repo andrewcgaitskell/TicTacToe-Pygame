eeeb4345878e50ffc8cb2fd54937829b882e7ceb
andrewcgaitskell <andrew@gaitskell.com>
2021-02-13 11:59:29 +0000

    Update server.py

--- gunicorn/websock1/flask/server.py
+++ gunicorn/websock1/flask/server.py
@@ -29,7 +29,5 @@ def my_custom_event(arg1, arg2, arg3):
 def handle_my_custom_namespace_event(json):
     print('received json: ' + str(json))
 
- 
-
-if __name__ == '__main__':
-    socketio.run(app)
+#if __name__ == '__main__':
+#    socketio.run(app)
