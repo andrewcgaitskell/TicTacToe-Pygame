082d3205685402a6f4c63f4145184ba950f2618a
andrewcgaitskell <andrew@gaitskell.com>
2021-02-15 16:27:37 +0000

    Update route_decorator.py

--- gunicorn/websock1/flasksockets/route_decorator.py
+++ gunicorn/websock1/flasksockets/route_decorator.py
@@ -21,6 +21,6 @@ def hello():
 if __name__ == "__main__":
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('', 5008), app, handler_class=WebSocketHandler)
     server.serve_forever()
     
