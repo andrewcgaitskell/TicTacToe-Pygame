fd0c9fd7fca5367a81c0091815398c0fb7fd63f1
andrewcgaitskell <andrew@gaitskell.com>
2021-02-16 09:52:42 +0000

    Update and rename client.py to echo_client.py

--- gunicorn/websocket1/echo_client.py
+++ gunicorn/websocket1/echo_client.py
@@ -1,5 +1,6 @@
 from websocket import create_connection
-ws = create_connection("ws://echo.websocket.org/")
+uri = "ws://player2.acgtest.info:5008/echo"
+ws = create_connection(uri)
 print("Sending 'Hello, World'...")
 ws.send("Hello, World")
 print("Sent")
