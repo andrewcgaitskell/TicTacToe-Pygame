29e26af9bae305796af76697510591b2bb84648f
andrewcgaitskell <andrew@gaitskell.com>
2021-02-20 19:26:13 +0000

    Update gevent.sh

--- gunicorn/flaskwebsockets/gevent.sh
+++ gunicorn/flaskwebsockets/gevent.sh
@@ -1,1 +1,1 @@
-uwsgi --master --http 0.0.0.0:5008 --http-websockets --gevent 100 --wsgi server:app
+uwsgi --master --http 0.0.0.0:5008 --socket 0.0.0.0:5008 --http-websockets --gevent 100 --wsgi server:app
