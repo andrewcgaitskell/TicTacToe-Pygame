1d728229f06e7b6e6df1591b3ec75255a6a991c3
andrewcgaitskell <andrew@gaitskell.com>
2021-02-21 11:33:48 +0000

    Update Hello2Seconds.py

--- uwsgi/websockets/asyncio/Hello2Seconds.py
+++ uwsgi/websockets/asyncio/Hello2Seconds.py
@@ -6,4 +6,4 @@ def two_seconds_elapsed():
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     asyncio.get_event_loop().call_later(2, two_seconds_elapsed)
-    return [b"Hello World"]
+    return [b"Hello World 12345"]
