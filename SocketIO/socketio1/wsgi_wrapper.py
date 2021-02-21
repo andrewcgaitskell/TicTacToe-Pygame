de4d47398594c20eb6e42ade6c6d0f297082d9f9
andrewcgaitskell <andrew@gaitskell.com>
2021-02-21 16:26:39 +0000

    Update wsgi_wrapper.py

--- SocketIO/socketio1/wsgi_wrapper.py
+++ SocketIO/socketio1/wsgi_wrapper.py
@@ -4,7 +4,10 @@ from foobar import application
 
 wsgi_handler = WSGIHandler(application)
 
-app = web.Application()
-app.router.add_route("*", "/{path_info:.*}", wsgi_handler)
-
+def init_func(argv):
+    app = web.Application()
+    app.router.add_get("/", index_handler)
+    app.router.add_route("*", "/{path_info:.*}", wsgi_handler)
+    return app
+    
 web.run_app(app)
