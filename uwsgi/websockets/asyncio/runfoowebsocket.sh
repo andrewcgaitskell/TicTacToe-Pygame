80dc49347b4a7a3d9e3804ec2311b2071a6bb817
andrewcgaitskell <andrew@gaitskell.com>
2021-02-21 12:39:11 +0000

    Update runfoowebsocket.sh

--- uwsgi/websockets/asyncio/runfoowebsocket.sh
+++ uwsgi/websockets/asyncio/runfoowebsocket.sh
@@ -1,1 +1,3 @@
-uwsgi --http-socket 0.0.0.0:5010 --master --async 10 --wsgi-file foowebsocket.py --ugreen
+#uwsgi --http-socket 0.0.0.0:5010 --master --async 10 --wsgi-file foowebsocket.py --ugreen
+
+uwsgi --master --async 10 --wsgi-file foowebsocket.py --ugreen
