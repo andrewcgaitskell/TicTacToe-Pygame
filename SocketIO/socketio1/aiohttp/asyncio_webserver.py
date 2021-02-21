6d92dee2fc72fc43b6b5d1da3d351ab163c9acfa
andrewcgaitskell <andrew@gaitskell.com>
2021-02-21 19:49:20 +0000

    Update asyncio_webserver.py

--- SocketIO/socketio1/aiohttp/asyncio_webserver.py
+++ SocketIO/socketio1/aiohttp/asyncio_webserver.py
@@ -32,22 +32,27 @@ app = web.Application()
 app.router.add_route('GET', '/', hello)
 app.router.add_route('GET', '/ws/', websocket_handler)
 
-loop = asyncio.get_event_loop()
-handler = app.make_handler()
+#loop = asyncio.get_event_loop()
+#handler = app.make_handler()
 ####
 
 
-f = loop.create_server(handler, '0.0.0.0', 5010)
-srv = loop.run_until_complete(f)
-print('serving on', srv.sockets[0].getsockname())
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-finally:
-    loop.run_until_complete(handler.finish_connections(1.0))
-    srv.close()
-    loop.run_until_complete(srv.wait_closed())
-    loop.run_until_complete(app.finish())
-loop.close()
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    loop = asyncio.get_event_loop()
+    handler = app.make_handler()
+    f = loop.create_server(handler, '0.0.0.0', 5010)
+    srv = loop.run_until_complete(f)
+    print('serving on', srv.sockets[0].getsockname())
+    
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(handler.finish_connections(1.0))
+        srv.close()
+        loop.run_until_complete(srv.wait_closed())
+        loop.run_until_complete(app.finish())
+    loop.close()
