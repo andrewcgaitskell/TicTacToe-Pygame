d2573bc71462cf6b2cea8e30ac4a2219690f38b9
andrewcgaitskell <andrew@gaitskell.com>
2021-02-21 16:28:54 +0000

    Update wsgi_wrapper.py

--- SocketIO/socketio1/wsgi_wrapper.py
+++ SocketIO/socketio1/wsgi_wrapper.py
@@ -8,6 +8,7 @@ def init_func(argv):
     app = web.Application()
     app.router.add_get("/", index_handler)
     app.router.add_route("*", "/{path_info:.*}", wsgi_handler)
+    web.run_app(app)
     return app
     
-web.run_app(app)
+
